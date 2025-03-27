cbf038209050c491d50c90507661f4294a49e0d4
jhs1336 <joshua.holzman.sharfe@gmail.com>
2025-03-27 16:31:08 -0400

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.tomotives.tomotives {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.tomotives.tomotives to javafx.fxml;
+    exports com.tomotives.tomotives;
+}
