2f7e20b0162b30166b56a31cea782487df4418c8
jhs1336 <joshua.holzman.sharfe@gmail.com>
2025-04-06 13:24:06 -0400

    small location display start

--- src/main/java/com/tomotives/tomotives/TOMotivesApplication.java
+++ src/main/java/com/tomotives/tomotives/TOMotivesApplication.java
@@ -13,11 +13,11 @@ public class TOMotivesApplication extends Application {
     public void start(Stage stage) {
         try {
             // load xml
-            FXMLLoader loader = new FXMLLoader(TOMotivesApplication.class.getResource("sign-up.fxml"));
+            FXMLLoader loader = new FXMLLoader(TOMotivesApplication.class.getResource("home.fxml"));
 
             // DIMENSIONS: 1280 x 720
             Scene scene = new Scene(loader.load(), 1280, 720);
-            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+            scene.getStylesheets().add(getClass().getResource("small-location-display.css").toExternalForm());
             // set up options
             stage.setTitle("TOMotives");
             stage.setScene(scene);
