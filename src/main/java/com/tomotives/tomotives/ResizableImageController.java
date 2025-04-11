df0463ec53e93a1326c697cbc944e2969c198f04
jhs1336 <joshua.holzman.sharfe@gmail.com>
2025-04-11 00:09:42 -0400

    resizable image class start

---
+++ src/main/java/com/tomotives/tomotives/ResizableImageController.java
@@ -0,0 +1,58 @@
+package com.tomotives.tomotives;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+
+public class ResizableImageController {
+    @FXML
+    ImageView imageView;
+    Image image;
+
+    @FXML
+    public void initialize() {
+        image = imageView.getImage();
+        resize(425, 300);
+    }
+
+    public void resize(double width, double height) {
+        double targetAspectRatio = width / height;
+        double imageWidth = image.getWidth();
+        double imageHeight = image.getHeight();
+        double currentAspectRatio = imageWidth / imageHeight;
+
+        double cropX = 0;
+        double cropY = 0;
+        double cropWidth = imageWidth;
+        double cropHeight = imageHeight;
+
+        if (currentAspectRatio > targetAspectRatio) {
+            // image is wider than needed
+            cropWidth = imageHeight * targetAspectRatio;
+            cropX = (imageWidth - cropWidth) / 2;
+        } else if (currentAspectRatio < targetAspectRatio) {
+            // image is taller than needed
+            cropHeight = imageWidth / targetAspectRatio;
+            cropY = (imageHeight - cropHeight) / 2;
+        }
+
+        Rectangle2D viewport = new Rectangle2D(cropX, cropY, cropWidth, cropHeight);
+        imageView.setViewport(viewport);
+
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+
+        System.out.println("resized");
+    }
+}
