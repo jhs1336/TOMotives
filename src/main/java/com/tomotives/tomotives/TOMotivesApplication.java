6560e8ad715376001a0057164b8e159c95993b5a
saulmes13 <saul.mesbur@student.tdsb.on.ca>
2025-04-15 12:40:39 -0400

    Changed required password length text

--- src/main/java/com/tomotives/tomotives/TOMotivesApplication.java
+++ src/main/java/com/tomotives/tomotives/TOMotivesApplication.java
@@ -14,10 +14,10 @@ public class TOMotivesApplication extends Application {
     public void start(Stage stage) {
         try {
             // load xml
-            FXMLLoader loader = new FXMLLoader(TOMotivesApplication.class.getResource("home.fxml"));
+            FXMLLoader loader = new FXMLLoader(TOMotivesApplication.class.getResource("sign-up.fxml"));
 
-            // DIMENSIONS: 1280 x 720
-            scene = new Scene(loader.load(), 1280, 720);
+            // DIMENSIONS: 1920 x 1080
+            scene = new Scene(loader.load(), 1920, 1080);
             // set up options
             stage.setTitle("TOMotives");
             stage.setScene(scene);
