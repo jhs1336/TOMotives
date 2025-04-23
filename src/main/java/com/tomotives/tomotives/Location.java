19167a0ba967026d6989b1a34a5cf65d5bda5531
saulmes13 <saul.mesbur@student.tdsb.on.ca>
2025-04-23 14:04:52 -0400

    getters + setters for location and user classes

--- src/main/java/com/tomotives/tomotives/Location.java
+++ src/main/java/com/tomotives/tomotives/Location.java
@@ -1,4 +1,71 @@
 package com.tomotives.tomotives;
 
+import java.util.ArrayList;
+
 public class Location {
+
+    // fields: name categories rating price description reviews
+    private String name;
+    private ArrayList<String> categories;
+    private double rating;
+    private double price;
+    private String description;
+    private ArrayList<String> reviews;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(ArrayList<String> categories) {
+        this.categories = categories;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(ArrayList<String> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- src/main/java/com/tomotives/tomotives/User.java
+++ src/main/java/com/tomotives/tomotives/User.java
@@ -1,4 +1,15 @@
 package com.tomotives.tomotives;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String displayName;
+    private ArrayList<Location> favourites;
+    private ArrayList<Location> recentLocations;
+    private ArrayList<User> friends;
 }
