0959617fe201ddd13d52479ca1ee295ae843fb14
jhs1336 <joshua.holzman.sharfe@gmail.com>
2025-05-28 18:23:31 -0400

    data fills on profile

--- src/main/java/com/tomotives/tomotives/controllers/ProfileController.java
+++ src/main/java/com/tomotives/tomotives/controllers/ProfileController.java
@@ -1,4 +1,30 @@
 package com.tomotives.tomotives.controllers;
 
+import com.tomotives.tomotives.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ProfileController {
+    @FXML
+    TextField displayNameTextField;
+    @FXML
+    TextField emailTextField;
+    @FXML
+    TextField firstNameTextField;
+    @FXML
+    TextField lastNameField;
+    @FXML
+    PasswordField passwordTextField;
+
+    @FXML
+    private void initialize() {
+        displayNameTextField.setText(Application.getUser().getDisplayName());
+        emailTextField.setText(Application.getUser().getEmail());
+        firstNameTextField.setText(Application.getUser().getFirstName());
+        lastNameField.setText(Application.getUser().getLastName());
+        passwordTextField.setText(Application.getUser().getPassword());
+
+
+    }
 }
